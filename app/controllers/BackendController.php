72ba6b1ad66023cb146d7451224a537f2f8ecb5c
anthony <anthonytrading81@gmail.com>
2014-06-16 13:51:45 +0800

    Add rest of stock history csv files

--- app/controllers/BackendController.php
+++ app/controllers/BackendController.php
@@ -4,60 +4,55 @@ class BackendController extends BaseController {
 
     public function store_streak()
     {
-        $days = DB::table('summaries')
-            ->select(['date', 'close', 'symbol'])
-            ->where('symbol', 'ABC')
-            ->orderBy('date', 'desc')
-            ->get();
+        $stocks = DB::table('stocks')->select('symbol')->get();
 
-        $streak = 0;
-        $iteration_price = null;
-
-        for ($i = 0; $i < count($days); $i++)
+        foreach ($stocks as $stock)
         {
-            // if the next days price is the same as the current, the streak is over
-            if ($days[$i + 1]->close === $days[$i]->close)
-            {
-                break;
-            }
+            $days = DB::table('summaries')
+                ->select(['date', 'close', 'symbol'])
+                ->where('symbol', $stock->symbol)
+                ->orderBy('date', 'desc')
+                ->limit(20)
+                ->get();
 
-            // if it's not the first iteration, check if the streak is over or not
-            if (! is_null($iteration_price))
+            $streak = 0;
+            $iteration_price = null;
+
+            for ($i = 0; $i < count($days); $i++)
             {
-                if ($streak > 0 && $days[$i + 1]->close > $iteration_price)
+                // if the next days price is the same as the current, the streak is over
+                if ($days[$i + 1]->close === $days[$i]->close) break;
+
+                // if it's not the first iteration, check if the streak is over or not
+                if (! is_null($iteration_price))
                 {
-                    // winning streak is over
-                    break;
+                    if ( $streak > 0 && $days[$i + 1]->close > $iteration_price ||
+                         $streak < 0 && $days[$i + 1]->close < $iteration_price
+                    ) break;
                 }
-                elseif ($streak < 0 && $days[$i + 1]->close < $iteration_price)
+
+                if ($days[$i + 1]->close > $days[$i]->close)
                 {
-                    // losing streak is over
-                    break;
+                    // the losing streak continues
+                    $streak--;
                 }
-            }
+                elseif ($days[$i + 1]->close < $days[$i]->close)
+                {
+                    // the winning streak continues
+                    $streak++;
+                }
 
-            if ($days[$i + 1]->close > $days[$i]->close) // on a losing streak
-            {
-                $streak--;
-                pp('subtracting 1. streak is: ' . $streak);
-
-                // update iteration price with new value
+                // store the next's days closing price
+                // on the following iteration, this price will be the current day price
+                // and we will compare against the new next day price
                 $iteration_price = $days[$i + 1]->close;
             }
-            elseif ($days[$i + 1]->close < $days[$i]->close) // on a winning streak
-            {
-                $streak++;
-                pp('adding 1. streak is: ' . $streak);
 
-                // update iteration price with new value
-                $iteration_price = $days[$i + 1]->close;
-            }
-            else
-            {
-                throw new Exception("Problem with closing summary data. Stock: " . $days[$i]->symbol);
-            }
+            Stock::where('symbol', $stock->symbol)->update([
+                'streak' => $streak,
+                'updated_at' => new Datetime,
+            ]);
         }
-        pp('The streak is: ' . $streak);
     }
 
     public function store_historical_data()
@@ -66,9 +61,20 @@ class BackendController extends BaseController {
 
         $daily_summaries = [];
 
+        $summaries = DB::table('summaries')->select('symbol')->groupBy('symbol')->get();
+
+        $symbols = array_pluck($summaries, 'symbol');
+
         foreach ($files as $file)
         {
+            $symbol = basename($file, '.csv');
+
+            if (in_array($symbol, $symbols)) continue;
+
             $handle = fopen($file, "r");
+
+            // reset the stocks summaries array with each iteration
+            $daily_summaries = [];
 
             while( ! feof($handle))
             {
@@ -78,33 +84,32 @@ class BackendController extends BaseController {
                 if ($daily_summary[0] !== 'Date')
                 {
                     $daily_summary[7] = basename($file, '.csv');
-                    $daily_summaries[] = $daily_summary;
                 }
+
+                $daily_summaries[] = $daily_summary;
             }
 
             fclose($handle);
-        }
-
-        Summary::truncate();
-
-        foreach($daily_summaries as $summary)
-        {
-            // validate the array by checking if it has 8 elements
-            if (count($summary) == 8)
+    
+            foreach($daily_summaries as $summary)
             {
-                Summary::create(
-                    [
-                        'date'   => $summary[0],
-                        'symbol' => $summary[7],
-                        'open'   => $summary[1],
-                        'high'   => $summary[2],
-                        'low'    => $summary[3],
-                        'close'  => $summary[4],
-                        'adjusted_close' => $summary[6],
-                        'volume' => $summary[5],
-                        'updated_at' => new Datetime,
-                    ]
-                );
+                // validate the array by checking if it has 8 elements
+                if (count($summary) == 8)
+                {
+                    Summary::create(
+                        [
+                            'date'   => $summary[0],
+                            'symbol' => $summary[7],
+                            'open'   => $summary[1],
+                            'high'   => $summary[2],
+                            'low'    => $summary[3],
+                            'close'  => $summary[4],
+                            'adjusted_close' => $summary[6],
+                            'volume' => $summary[5],
+                            'updated_at' => new Datetime,
+                        ]
+                    );
+                }
             }
         }
 
