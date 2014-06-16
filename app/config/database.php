b8cd94b91898c583a3dd451df36bcba124f14f1a
anthony <anthonytrading81@gmail.com>
2014-06-16 14:15:32 +0800

    Add production db config

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
+			'database'  => 'stockstreaks',
 			'username'  => 'forge',
-			'password'  => '',
+			'password'  => '8vcSGibq9YlS45121HSM',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
