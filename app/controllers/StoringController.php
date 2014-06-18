c20957d8f7e2b2340e52ca184e050027367376ba
anthony <anthonytrading81@gmail.com>
2014-06-18 21:55:59 +0800

    make store streak faster

--- app/controllers/StoringController.php
+++ app/controllers/StoringController.php
@@ -4,7 +4,7 @@ class StoringController extends BaseController {
 
     public function store_streaks()
     {
-        // ini_set("memory_limit", "-1");
+        ini_set("memory_limit", "-1");
         set_time_limit(0);
 
         $date = date('Y-m-d');
@@ -13,6 +13,13 @@ class StoringController extends BaseController {
 
         foreach ($stocks as $stock)
         {
+            $stored = DB::table('stocks')
+                ->where('symbol', $stock->symbol)
+                ->where('streak_stored', $date)
+                ->first();
+
+            if ($stored) continue;
+                
             $days = DB::table('summaries')
                 ->select('close', 'date')
                 ->where('symbol', $stock->symbol)
@@ -22,58 +29,48 @@ class StoringController extends BaseController {
 
             $streak = 0;
 
-            $stored = DB::table('stocks')
-                ->where('symbol', $stock->symbol)
-                ->where('streak_stored', $date)
-                ->first();
-
-            if (is_null($stored))
+            for ($i = 0; $i < count($days); $i++)
             {
-                for ($i = 0; $i < count($days); $i++)
+                // if we're at the earliest day recorded for a stock, the streak is over
+                if ( ! isset($days[$i + 1])) break;
+
+                // if the next days price is the same as the current, the streak is over
+                if ($days[$i]->close === $days[$i + 1]->close) break;
+
+                // one time check for the first iteration
+                if ($i === 0)
                 {
-                    // if we're at the earliest day recorded for a stock, the streak is over
-                    if ( ! isset($days[$i + 1])) break;
-
-                    // if the next days price is the same as the current, the streak is over
-                    if ($days[$i]->close === $days[$i + 1]->close) break;
-
-                    // one time check for the first iteration
-                    if ($i === 0)
-                    {
-                        if ($days[$i] > $days[$i + 1]) $streak++; continue;
-                        if ($days[$i] < $days[$i + 1]) $streak--; continue;
-                    }
-
-                    // check if the winning streak is over or not
-                    if ($streak > 0)
-                    {
-                        // if current day's close is less than the day before it, the winning streak is over
-                        if ($days[$i]->close < $days[$i + 1]->close) break;
-                        
-                        // the winning streak continues
-                        $streak++;
-                    }
-                    elseif ($streak < 0)
-                    {
-                        // if the current day's close is more than the day before it, the losing streak is over
-                        if ($days[$i]->close > $days[$i + 1]->close) break;
-                        
-                        // the losing streak continues
-                        $streak--;
-                    }
+                    if ($days[$i] > $days[$i + 1]) $streak++; continue;
+                    if ($days[$i] < $days[$i + 1]) $streak--; continue;
                 }
 
-                DB::table('stocks')
-                    ->where('symbol', $stock->symbol)
-                    ->update(
-                        [
-                            'streak' => $streak,
-                            'streak_stored' => $date,
-                        ]
-                    );
-
-                print "Stored a streak of " . $streak . " for: " . $stock->symbol . "\n";
+                // check if the winning streak is over or not
+                if ($streak > 0)
+                {
+                    // if current day's close is less than the day before it, the winning streak is over
+                    if ($days[$i]->close < $days[$i + 1]->close) break;
+                    
+                    // the winning streak continues
+                    $streak++;
+                }
+                elseif ($streak < 0)
+                {
+                    // if the current day's close is more than the day before it, the losing streak is over
+                    if ($days[$i]->close > $days[$i + 1]->close) break;
+                    
+                    // the losing streak continues
+                    $streak--;
+                }
             }
+
+            DB::table('stocks')
+                ->where('symbol', $stock->symbol)
+                ->update([
+                    'streak' => $streak,
+                    'streak_stored' => $date,
+                ]);
+
+            print "Stored a streak of " . $streak . " for: " . $stock->symbol . "\n";
         }
     }
 
