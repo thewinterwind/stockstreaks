e08118ba283e51776cca708b79d046d8f1fde61d
anthony <anthonytrading81@gmail.com>
2014-06-21 04:32:07 +0800

    add analytics

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -21,5 +21,16 @@
     <script src="//cdn.datatables.net/1.10.0/js/jquery.dataTables.js"></script>
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
     <script src="/js/main.js"></script>
+    @if (App::environment() == 'production')
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-8595180-11', 'stockstreaks.com');
+        ga('send', 'pageview');
+    </script>
+    @endif
 </body>
 </html>
